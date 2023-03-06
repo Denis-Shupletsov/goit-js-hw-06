0fb48928b036d14b48773048c81ba0908c25a281
Denis-Shupletsov <b128k2351763@gmail.com>
2023-03-06 22:13:11 +0200

    4

--- js/task-04.js
+++ js/task-04.js
@@ -9,3 +9,19 @@
 //     Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
 // Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
 // Обновляй интерфейс новым значением переменной counterValue.
+
+const calculatorCounter = function ({
+    rootSelector, counterValue = 0, klik = 1 } = {}) {
+    this._rootSelector = rootSelector;
+    this._valeu = counterValue;
+    this._klik = klik;
+};
+
+calculatorCounter.prototype.increment = function () {
+    this._valeu += this._klik;
+};
+
+calculatorCounter.prototype.decrement = function () {
+    this._valeu -= this._klik;
+};
+const calculator = new calculatorCounter({ rootSelector: 'counter', klik: 1 });
