31c082087131478755c6e1947eeb17dc354e706f
Denis-Shupletsov <b128k2351763@gmail.com>
2023-03-07 08:41:54 +0200

    4 ok

--- js/task-04.js
+++ js/task-04.js
@@ -1,27 +1,21 @@
 // Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать и уменьшать его значение на единицу.
 
-// < div id = "counter" >
-//   <button type="button" data-action="decrement">-1</button>
-//   <span id="value">0</span>
-//   <button type="button" data-action="increment">+1</button>
-// </ >
-
 //     Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
 // Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
 // Обновляй интерфейс новым значением переменной counterValue.
 
-const calculatorCounter = function ({
-    rootSelector, counterValue = 0, klik = 1 } = {}) {
-    this._rootSelector = rootSelector;
-    this._valeu = counterValue;
-    this._klik = klik;
-};
+const currentValue = document.querySelector('#value');
+const decrement = document.querySelector('button[data-action="decrement"]');
+const increment = document.querySelector('button[data-action="increment"]');
 
-calculatorCounter.prototype.increment = function () {
-    this._valeu += this._klik;
-};
+let counterValue = 0;
 
-calculatorCounter.prototype.decrement = function () {
-    this._valeu -= this._klik;
-};
-const calculator = new calculatorCounter({ rootSelector: 'counter', klik: 1 });
+increment.addEventListener('click', () => {
+    counterValue += 1;
+    currentValue.textContent = counterValue;
+});
+
+decrement.addEventListener('click', () => {
+    counterValue -= 1;
+    currentValue.textContent = counterValue;
+});
